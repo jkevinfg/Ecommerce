8e25ecf1626865758f1668af1533be911036320b
JairKevinFG <jkfloresg@uni.pe>
2021-09-23 23:59:11 -0500

    pages not found

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+import React from 'react';
+import {BrowserRouter , Route ,Switch}from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
-   <div> Hola mundo desde React</div> 
+   <BrowserRouter>
+    <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/register" exact component={Register} />
+          <Route component={NotFound} />
+    </Switch>
+   </BrowserRouter>
   )
 }
 export default App;
