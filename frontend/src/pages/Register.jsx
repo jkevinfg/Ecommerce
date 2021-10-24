32d73c26d913b9ca932af47130bbc7df03d6b5ee
JairKevinFG <jkfloresg@uni.pe>
2021-10-24 14:35:51 -0500

    register bootstrap

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Register = () => {
 
-  return (
-    <div>
-      <p>
-        Register
-      </p>
-    </div>
+
+const [email,setEmail] = useState('');
+
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  console.log(email);
+
+}
+const handleChange=(event) => {
+  setEmail(event.target.value);
+}
+
+return (
+  <div className="container p-3">
+      <div className="row">
+            <div className="col-md-6 offset-md-3">
+                  <h4>Registro</h4>
+                  <form onSubmit={handleSubmit}>
+                      <input type="email" className="form-control" onChange={handleChange}/>
+                      <button type="submit" className="btn btn-success mt-2">Enviar</button>
+                  </form>
+            </div>
+         </div>
+  </div>
+
+
   )
+
 }
 
 export default Register;
