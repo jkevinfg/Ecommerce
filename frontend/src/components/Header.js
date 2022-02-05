49ab9e9714dcc8157ae9357ecae2dba3b5dee457
jkevinfg <jkfloresg@uni.pe>
2022-02-05 12:52:55 -0500

    user logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,13 @@
 import React , {useState} from 'react';
 import { Menu } from 'antd';
-import { HomeOutlined, UserAddOutlined,LoginOutlined,  SettingOutlined } from '@ant-design/icons';
+import { HomeOutlined, UserAddOutlined,LoginOutlined,
+  LogoutOutlined,
+  SettingOutlined } from '@ant-design/icons';
 
 import {Link } from 'react-router-dom';
+import { auth } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 
 const { SubMenu } = Menu;
@@ -10,11 +15,23 @@ const { SubMenu } = Menu;
 
 const Header = () => {
 
-  const [current , setCurrent ] = useState("home")
+  const [current , setCurrent ] = useState("home");
+  let dispatch = useDispatch();
+  let history = useHistory();
 
   const handleClick =(event) => {
     setCurrent(event.key)
   }
+
+  const logout = () => {
+    auth.signOut();
+    dispatch({ 
+      type : 'LOGOUT',
+      payload : null
+    });
+    history.push('/login');
+  }
+
   return(
     <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
       <Menu.Item key="home" icon={<HomeOutlined />} className="me-auto">
@@ -28,7 +45,9 @@ const Header = () => {
       </Menu.Item>
       <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
           <Menu.Item key="opcion1">opcion1</Menu.Item>
-          <Menu.Item key="logout">Cerrar Sesión</Menu.Item>
+          <Menu.Item icon={<LogoutOutlined/>} key="logout"
+            onClick={logout}
+          >Cerrar Sesión</Menu.Item>
       </SubMenu>
 
     </Menu>
