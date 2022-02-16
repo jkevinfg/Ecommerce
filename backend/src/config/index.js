619e10a9d7b4de7a6fcc8976fe98de092fe681ed
jkevinfg <jkfloresg@uni.pe>
2022-02-16 01:04:10 -0500

    env example

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -4,7 +4,17 @@ const config = {
     dev: process.env.NODE_ENV !== 'production',
     port: process.env.PORT || 3000,
     cors: process.env.CORS,
-    database : process.env.DATABASE
+    database : process.env.DATABASE,
+    firebaseType : process.env.FIREBASE_SERVICE_TYPE ,
+    firebaseProjectId : process.env.FIREBASE_PROJECT_ID, 
+    firebasePrivateKeyId : process.env.FIREBASE_PRIVATE_KEY_ID,
+    firebasePrivateKey : process.env.FIREBASE_PRIVATE_KEY ,
+   firebaseClientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    firebaseClientId: process.env.FIREBASE_CLIENT_ID ,
+    firebaseAuthUri: process.env.FIREBASE_AUTH_URI,
+    firebaseTokenUri: process.env.FIREBASE_TOKEN_URI,
+    firebaseAuthProvider: process.env.FIREBASE_PROVIDER_URL,
+    firebaseClientUrl: process.env.FIREBASE_CLIENT_CERT_URL
 };
 
 module.exports = { config };
--- backend/src/utils/firebase/index.js
+++ backend/src/utils/firebase/index.js
@@ -1,7 +1,20 @@
-
 const admin = require("firebase-admin");
+const { config } = require('./src/config/index');
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = {
+    type: config.firebaseType,
+    project_id: config.firebaseProjectId,
+    private_key_id: config.firebasePrivateKeyId,
+    private_key: config.firebasePrivateKey,
+    client_email: config.firebaseClientEmail,
+    client_id: config.firebaseClientId,
+    auth_uri: config.firebaseAuthUri,
+    token_uri: config.firebaseTokenUri,
+    auth_provider_x509_cert_url: config.firebaseAuthProvider,
+    client_x509_cert_url: config.firebaseClientUrl
+};
+
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
