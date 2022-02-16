1bf4be3d0a7dc2996c7c3e863a3d673961a55512
jkevinfg <jkfloresg@uni.pe>
2022-02-15 23:59:42 -0500

    middleware

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,15 +1,29 @@
 const express = require('express');
 
 
+//middlewares
+const {authCheck} = require('../utils/middleware/auth'); 
+
+const myMiddleware = (req,res,next) => {
+    console.log('Im a Middleware yay')
+    next();
+}
+
 function authApi(app) {
     const router = express.Router();
     app.use('/api/auth', router);
   
-    router.get('/create-or-update-user',(req,res) => {
+    router.post('/create-or-update-user',authCheck, (req,res) => {
         res.json({
             data : "API nodejs"
         })
     });
+
+    router.get('/test',myMiddleware, (req,res) => {
+        res.json({
+            data : "API nodejs"
+        })
+    })
   }
   
   module.exports = authApi;
--- backend/src/utils/firebase/index.js
+++ backend/src/utils/firebase/index.js
@@ -7,4 +7,7 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
+module.exports = admin;
+
+
 
