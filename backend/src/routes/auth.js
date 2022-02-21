6e05cfa7e81308637285109f161e1892a7ea29a2
jkevinfg <jkfloresg@uni.pe>
2022-02-21 00:33:33 -0500

    create or update user

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,21 +1,30 @@
 const express = require('express');
-
+const User = require('../utils/schemas/user');
 
 //middlewares
 const {authCheck} = require('../utils/middleware/auth'); 
-
 
 function authApi(app) {
     const router = express.Router();
     app.use('/api/auth', router);
   
-    router.post('/create-or-update-user',authCheck, (req,res) => {
-        res.json({
-            data : "API nodejs"
-        })
+    //POST
+    router.post('/create-or-update-user',authCheck, async (req,res) => {
+        const {email,aud} = req.user;
+        console.log(req.user)
+        const user = await User.findOneAndUpdate({email}, {name:aud},{new : true});
+        if(user){
+            console.log('User updated',user)
+            res.json(user);
+        }else {
+            const newUser = await new User({
+                email,
+                name: aud, 
+            }).save();
+            console.log('User created',newUser)
+            res.json(newUser);
+        }
     });
-
- 
   }
   
   module.exports = authApi;
--- backend/src/utils/middleware/auth.js
+++ backend/src/utils/middleware/auth.js
@@ -1,13 +1,13 @@
 const admin = require('../firebase');
 
-
-exports.authCheck = (req,res,next) => {
-    console.log(req.headers); //token
+exports.authCheck = async (req,res,next) => {
+   // console.log(req.headers); //token
     try {
-        const firebaseUser = admin.auth().verifyIdToken(req.headers.authtoken)
-        console.log(req.headers.authtoken)
+        const firebaseUser = await admin.auth().verifyIdToken(req.headers.authtoken)
+      //  console.log("Firebase user in authcheck",firebaseUser);
+        req.user = firebaseUser;
+        next();
     }catch(err) {
-        console.log(err)
         res.status(401).json({
             err : 'Token invalido o expirado'
         })
